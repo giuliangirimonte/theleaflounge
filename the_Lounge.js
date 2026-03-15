23bd0efce7bc1dbc4737b6973a3f3ce58e5240e1
Giulian Girimonte <giuliangirimonte@Giulians-MacBook-Pro.local>
2026-03-14 20:33:57 -0400

    the garden button fix, updates to the lounge

--- the_Lounge.js
+++ the_Lounge.js
@@ -1,3 +1,18 @@
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    if (navLinks.classList.contains("active")) {
+      hamburger.textContent = "☰";
+    } else {
+      hamburger.textContent = "∷";
+    }
+  });
+
+
+// clock widget
+
 function updateClock() {
   const now = new Date();
   const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
@@ -6,4 +21,36 @@ function updateClock() {
 
 updateClock();
 setInterval(updateClock, 1000);
+
+// audio widget
+function toggleSound(id){
+
+let audio = document.getElementById(id);
+
+if(audio.paused){
+audio.play();
+}else{
+audio.pause();
+}
+}
+
+// tips widget
+const tips = [
+"Rotate your plants weekly.",
+"Misting doesn't replace humidity.",
+"Check soil before watering.",
+"Bright indirect light is ideal for many plants.",
+"Yellow leaves can mean overwatering.",
+"Wipe dust off leaves so plants can photosynthesize better."
+];
+
+function updateTip() {
+  const randomTip = tips[Math.floor(Math.random() * tips.length)];
+  document.getElementById("plantTip").textContent = randomTip;
+}
+
+updateTip();
+setInterval(updateTip, 15000);
+
+
 
